a4acda21be2b1eebdd161737b960fed747a95e90
Enrique Pittaluga <ep7m@me.com>
2020-04-10 17:35:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,28 @@
 # Write your code here!
+
+def game_hash
+  
+  hash = {
+    :home => {
+      :team_name => "",
+      :color => [],
+      :players => [],
+    },
+    :away => {
+      :team_name => "",
+      :color => [],
+      :players => [{
+        :player_name => "",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      }, ],
+    }
+  }
+  
+end
