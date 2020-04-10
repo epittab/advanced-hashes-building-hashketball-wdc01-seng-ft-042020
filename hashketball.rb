205b5454117ed47437da58d35c6e7f9640092770
Enrique Pittaluga <ep7m@me.com>
2020-04-10 17:53:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
   hash = {
     :home => {
       :team_name => "Brooklyn Nets",
-      :color => ["Black", "White"],
+      :colors => ["Black", "White"],
       :players => [{
         :player_name => "Alan Anderson",
         :number => 0,
@@ -64,7 +64,7 @@ def game_hash
     },
     :away => {
       :team_name => "Charlotte Hornets",
-      :color => ["Turquoise", "Purple"],
+      :colors => ["Turquoise", "Purple"],
       :players => [{
         :player_name => "Jeff Adrien",
         :number => 4,
