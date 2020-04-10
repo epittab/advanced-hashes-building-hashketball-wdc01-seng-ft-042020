3ff20ef30346d68e7d7f09f4eb119876b228408e
Enrique Pittaluga <ep7m@me.com>
2020-04-10 17:38:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,10 +6,64 @@ def game_hash
     :home => {
       :team_name => "",
       :color => [],
-      :players => [],
+      :players => [{
+        :player_name => "Jeff Adrien",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      },{
+        :player_name => "Bismack Biyombo",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      },
+      {
+        :player_name => "DeSagna Diop",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      },
+      {
+        :player_name => "Ben Gordon",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      },
+      {
+        :player_name => "Kemba Walker",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      }
+      ]
     },
     :away => {
-      :team_name => "",
+      :team_name => "Charlotte Hornets",
       :color => ["Turquoise", "Purple"],
       :players => [{
         :player_name => "Jeff Adrien",
